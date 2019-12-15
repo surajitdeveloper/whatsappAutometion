9ac590e00f978ec59aab8acd4293103441793157
Surajit Sadhukhan <surajit1992@live.com>
2019-12-15 16:27:28 +0530

    first commit

---
+++ Whatsapp Automation/new_wa.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+import mysql.connector
+#enter perfect name as in your contact (it is case sensitive)
+# name=input("to whom you want to send message?")
+#replace /chromedriver with loaction of chromedriver on your computer
+
+class Whatsapp(object):
+    """__init__() functions as the class constructor"""
+    def __init__(self, id=None, mobile=None, message=None):
+        self.id = id
+        self.mobile = mobile
+        self.message = message
+
+
+cnx = mysql.connector.connect(
+    host="78.47.90.52",
+    port=3306,
+    user="root",
+    password="Surajit@123",
+    database="whatsapp")
+
+
+
+def start_wp(numbers):
+
+    print(numbers)
+    return False
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    elem = browser.find_element_by_xpath("//input[@title='Search or start new chat']")
+    elem.click()
+    elem.send_keys(name)
+    time.sleep(10)
+    # browser.find_element_by_xpath("//*[@title='"+name+"']").click()
+    try:
+        browser.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click()
+        time.sleep(5)
+    except:
+        browser.find_element_by_xpath(
+            '//div[@style="z-index: 101; transition: none 0s ease 0s; height: 72px; transform: translateY(72px);"]').click()
+        time.sleep(5)
+
+    elem = browser.find_element_by_xpath("//div[@data-tab='1']")
+    elem.click()
+    elem.send_keys(message + Keys.ENTER)
+
+def start_surf(cnx):
+    mycursor = cnx.cursor()
+    mycursor.execute("SELECT * FROM whatsapp where status = 'pending' limit 100")
+
+    myresult = mycursor.fetchall()
+    number_list = []
+    for x in myresult:
+        number_list.append(Whatsapp(x[0], x[1], x[4]))
+    # print(number_list[5].message)
+    # print(number_list[5].id)
+    start_wp(number_list, cnx)
+    time.sleep(60)
+    start_surf(cnx)
+
+start_surf(cnx)
+exit(1)
+name='+91 85850 25948'
+message=""
+browser = webdriver.Chrome(executable_path='/run/media/surajit/962E95F42E95CE1D/Youtube/chromedriver')
+browser.get('https://web.whatsapp.com')
+time.sleep(60)
+
+# for i in range(20):
+#     elem.send_keys("hello this is from python " + Keys.ENTER)
+#     time.sleep(1)
+# browser.close()
