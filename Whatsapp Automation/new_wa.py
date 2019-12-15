b6d52bb787dcd425995986a046cf2c599a5ab6cc
Surajit Sadhukhan <surajit1992@live.com>
2019-12-15 16:53:11 +0530

    first commit

--- Whatsapp Automation/new_wa.py
+++ Whatsapp Automation/new_wa.py
@@ -6,6 +6,7 @@ import mysql.connector
 # name=input("to whom you want to send message?")
 #replace /chromedriver with loaction of chromedriver on your computer
 
+
 class Whatsapp(object):
     """__init__() functions as the class constructor"""
     def __init__(self, id=None, mobile=None, message=None):
@@ -14,38 +15,44 @@ class Whatsapp(object):
         self.message = message
 
 
-cnx = mysql.connector.connect(
-    host="78.47.90.52",
-    port=3306,
-    user="root",
-    password="Surajit@123",
-    database="whatsapp")
+def start_wp(numbers, cnx, browser):
+    # print(numbers)
+    for i in range(0, len(numbers)):
+        # print(i)
+        # print(numbers[i].message + " " + str(numbers[i].id))
+        name = numbers[i].mobile
+        id = numbers[i].id
+        message = numbers[i].message
+        now = time.strftime('%Y-%m-%d %H:%M:%S')
+        elem = browser.find_element_by_xpath("//input[@title='Search or start new chat']")
+        elem.click()
+        elem.send_keys(name)
+        time.sleep(10)
+        # browser.find_element_by_xpath("//*[@title='"+name+"']").click()
+        try:
+            browser.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click()
+            time.sleep(5)
+        except:
+            browser.find_element_by_xpath(
+                '//div[@style="z-index: 101; transition: none 0s ease 0s; height: 72px; transform: translateY(72px);"]').click()
+            time.sleep(5)
+
+        try:
+            elem = browser.find_element_by_xpath("//div[@data-tab='1']")
+            elem.click()
+            elem.send_keys(message + Keys.ENTER)
+            sql = "UPDATE whatsapp SET status = 'successful', update_on = '"+now+"' WHERE id = '"+str(id)+"'"
+            mycursor = cnx.cursor()
+            mycursor.execute(sql)
+            cnx.commit()
+        except:
+            sql = "UPDATE whatsapp SET status = 'failed', update_on = '" + now + "' WHERE id = '" + str(id) + "'"
+            mycursor = cnx.cursor()
+            mycursor.execute(sql)
+            cnx.commit()
 
 
-
-def start_wp(numbers):
-
-    print(numbers)
-    return False
-    now = time.strftime('%Y-%m-%d %H:%M:%S')
-    elem = browser.find_element_by_xpath("//input[@title='Search or start new chat']")
-    elem.click()
-    elem.send_keys(name)
-    time.sleep(10)
-    # browser.find_element_by_xpath("//*[@title='"+name+"']").click()
-    try:
-        browser.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click()
-        time.sleep(5)
-    except:
-        browser.find_element_by_xpath(
-            '//div[@style="z-index: 101; transition: none 0s ease 0s; height: 72px; transform: translateY(72px);"]').click()
-        time.sleep(5)
-
-    elem = browser.find_element_by_xpath("//div[@data-tab='1']")
-    elem.click()
-    elem.send_keys(message + Keys.ENTER)
-
-def start_surf(cnx):
+def start_surf(cnx, browser):
     mycursor = cnx.cursor()
     mycursor.execute("SELECT * FROM whatsapp where status = 'pending' limit 100")
 
@@ -55,17 +62,26 @@ def start_surf(cnx):
         number_list.append(Whatsapp(x[0], x[1], x[4]))
     # print(number_list[5].message)
     # print(number_list[5].id)
-    start_wp(number_list, cnx)
+    start_wp(number_list, cnx, browser)
     time.sleep(60)
-    start_surf(cnx)
+    start_surf(cnx, browser)
 
-start_surf(cnx)
-exit(1)
-name='+91 85850 25948'
-message=""
+
+cnx = mysql.connector.connect(
+    host="78.47.90.52",
+    port=3306,
+    user="root",
+    password="Surajit@123",
+    database="whatsapp")
+
 browser = webdriver.Chrome(executable_path='/run/media/surajit/962E95F42E95CE1D/Youtube/chromedriver')
 browser.get('https://web.whatsapp.com')
 time.sleep(60)
+start_surf(cnx, browser)
+# exit(1)
+# name='+91 85850 25948'
+# message=""
+
 
 # for i in range(20):
 #     elem.send_keys("hello this is from python " + Keys.ENTER)
