176746b428bb967b3894eec4b3dd6a8a8633e6cb
Surajit Sadhukhan <surajit1992@live.com>
2019-12-16 09:21:00 +0530

    improving

--- Whatsapp Automation/new_wa.py
+++ Whatsapp Automation/new_wa.py
@@ -29,7 +29,7 @@ def start_wp(numbers, cnx, browser):
         elem.click()
         elem.send_keys(name)
         time.sleep(10)
-        print(id)
+        print("id - "+str(id))
         # browser.find_element_by_xpath("//*[@title='"+name+"']").click()
         try:
             browser.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click()
@@ -63,35 +63,36 @@ def start_wp(numbers, cnx, browser):
             print("failed")
 
 
-def start_surf(cnx, browser):
-    mycursor = cnx.cursor()
+def start_surf(browser):
+    try:
+        cnx = mysql.connector.connect(
+            host="78.47.90.52",
+            port=3306,
+            user="root",
+            password="Surajit@123",
+            database="whatsapp")
+        mycursor = cnx.cursor()
+    except Exception as e:
+        print("Invalid DB connection")
+        browser.close()
+        exit(1)
     print("Connecting DB...")
     print("Getting data...")
     mycursor.execute("SELECT * FROM whatsapp where status = 'pending' limit 100")
     myresult = mycursor.fetchall()
-    print("Got -" + str(len(myresult)) + " results")
+    print("Got - " + str(len(myresult)) + " records")
     if len(myresult) > 0:
         number_list = []
         for x in myresult:
             number_list.append(Whatsapp(x[0], x[1], x[5], x[2]))
         start_wp(number_list, cnx, browser)
-    start_surf(cnx, browser)
-
-
-try:
-    cnx = mysql.connector.connect(
-        host="78.47.90.52",
-        port=3306,
-        user="root",
-        password="Surajit@123",
-        database="whatsapp")
-except Exception as e:
-    print("Invalid DB connection");
-    exit(1)
+    print("Waiting for getting new data")
+    time.sleep(60)
+    start_surf(browser)
 
 
 browser = webdriver.Chrome(executable_path='/run/media/surajit/962E95F42E95CE1D/Youtube/chromedriver')
 browser.get('https://web.whatsapp.com')
 time.sleep(60) # uncomment it
-start_surf(cnx, browser)
+start_surf(browser)
 browser.close()
